6770203a2f9ab030afeca099c094cbc776537d07
Timo Wilhelm <mail@timowilhelm.com>
2025-06-24 22:17:33 +0200

    update endpoints

--- README.md
+++ README.md
@@ -12,3 +12,38 @@ Create a `.dev.vars` file with the following content:
 CLOUDFLARE_ACCOUNT_ID=<YOUR_CLOUDFLARE_ACCOUNT_ID>
 CLOUDFLARE_API_TOKEN=<YOUR_CLOUDFLARE_API_TOKEN>
 ```
+
+## Test
+
+You can send a request to `/upload` to upload a worker script to the Cloudflare Workers ofr Platforms namespace. To access this worker you must create a [dynamic dispatch Worker](https://developers.cloudflare.com/cloudflare-for-platforms/workers-for-platforms/get-started/dynamic-dispatch/).
+
+```curl
+POST https://localhost:8787.upload HTTP/1.1
+content-type: application/json
+
+{
+  "mainFileName": "index.js",
+  "files": [
+    {
+      "name": "index.js",
+      "content": <minified worker script>,
+      "type": "application/javascript+module"
+    }
+  ]
+}
+```
+
+A sample Worker script to test the D1 Database and Static Assets could look like this:
+
+```js
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === '/sql') {
+      const returnValue = await env.SQLITE.prepare(`SELECT date('now');`).run();
+      return Response.json(returnValue);
+    }
+    return env.ASSETS.fetch(request);
+  }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -2,112 +2,179 @@ import { Hono } from 'hono';
 import { AssetManifest, ScriptUpload } from './script-upload';
 import crypto from 'crypto';
 import { Resources } from './resources';
+import { createRoute, OpenAPIHono } from '@hono/zod-openapi';
+import { z } from 'zod';
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new OpenAPIHono<{ Bindings: Env }>({
+	strict: true,
+	defaultHook: (result, c) => {
+		if (!result.success) {
+			return c.json(
+				{
+					ok: false,
+					errors: result.error.format(),
+				},
+				422
+			);
+		}
+	},
+});
+
+app.notFound((c) => {
+	return c.json(
+		{
+			message: 'Not Found',
+		},
+		404
+	);
+});
+
+app.onError((err, c) => {
+	return c.json(
+		{
+			message: err.message,
+		},
+		500
+	);
+});
 
 const namespace = 'tiwi';
 const workerName = 'customer-worker-1';
 const user = 'customer-1';
 
-app.get('/', async (c) => {
-	const scriptUpload = new ScriptUpload(c.env.CLOUDFLARE_ACCOUNT_ID, c.env.CLOUDFLARE_API_TOKEN);
-
-	const sampleAssetContent = 'Howdy!';
-	const sampleAssetFileName = '/hello_world.txt'; // make sure this is a valid path beginning with `/`
-	const sampleAssetContentType = 'text/plain';
-
-	const sampleAssetBuffer = Buffer.from(sampleAssetContent);
-	const sampleAssetHash = crypto.createHash('sha256').update(sampleAssetContent).digest('hex').slice(0, 32);
-
-	const manifest = {
-		[sampleAssetFileName]: {
-			hash: sampleAssetHash,
-			size: sampleAssetBuffer.length,
+app.openapi(
+	createRoute({
+		method: 'post',
+		path: '/upload',
+		request: {
+			body: {
+				required: true,
+				content: {
+					'application/json': {
+						schema: z.object({
+							mainFileName: z.string(),
+							files: z.array(
+								z.object({
+									name: z.string(),
+									content: z.string(),
+									type: z.string(),
+								})
+							),
+						}),
+					},
+				},
+			},
 		},
-	} satisfies AssetManifest;
+		responses: {
+			200: {
+				description: 'OK',
+				content: {
+					'application/json': {
+						schema: z.object({
+							ok: z.boolean(),
+							errors: z.array(z.string()),
+						}),
+					},
+				},
+			},
+		},
+	}),
+	async (c) => {
+		const validatedBody = c.req.valid('json');
 
-	const uploadInfo = await scriptUpload.createAssetsUpload(namespace, workerName, manifest);
+		const scriptUpload = new ScriptUpload(c.env.CLOUDFLARE_ACCOUNT_ID, c.env.CLOUDFLARE_API_TOKEN);
 
-	let assetsToken: string | undefined;
+		/*
+		 * TODO: Here we are simulating uploading assets.
+		 * This should be done by creating an upload and passing the token to the user to upload the assets directly
+		 */
 
-	if (uploadInfo !== null) {
-		console.log('Uploading Assets');
+		const sampleAssetContent = 'Howdy!';
+		const sampleAssetFileName = '/hello_world.txt'; // make sure this is a valid path beginning with `/`
+		const sampleAssetContentType = 'text/plain';
 
-		try {
-			assetsToken = await scriptUpload.uploadFilesBatch(
-				uploadInfo,
-				new Map([
-					[
-						sampleAssetHash,
-						{
-							fileName: sampleAssetFileName,
-							data: sampleAssetBuffer,
-							type: sampleAssetContentType,
-						},
-					],
-				])
-			);
-		} catch (error) {
-			console.error(error);
-		}
-	}
+		const sampleAssetBuffer = Buffer.from(sampleAssetContent);
+		const sampleAssetHash = crypto.createHash('sha256').update(sampleAssetContent).digest('hex').slice(0, 32);
 
-	const worker = `
-	export default {
-		async fetch(request, env) {
-			const url = new URL(request.url);
-			if (url.pathname === '/sql') {
-				const returnValue = await env.SQLITE.prepare(\`SELECT date('now');\`).run();
-    			return Response.json(returnValue);
+		const manifest = {
+			[sampleAssetFileName]: {
+				hash: sampleAssetHash,
+				size: sampleAssetBuffer.length,
+			},
+		} satisfies AssetManifest;
+
+		const uploadInfo = await scriptUpload.createAssetsUpload(namespace, workerName, manifest);
+
+		let assetsToken: string | undefined;
+
+		if (uploadInfo !== null) {
+			console.log('Uploading Assets');
+
+			try {
+				assetsToken = await scriptUpload.uploadFilesBatch(
+					uploadInfo,
+					new Map([
+						[
+							sampleAssetHash,
+							{
+								fileName: sampleAssetFileName,
+								data: sampleAssetBuffer,
+								type: sampleAssetContentType,
+							},
+						],
+					])
+				);
+			} catch (error) {
+				console.error(error);
 			}
-			return env.ASSETS.fetch(request);
 		}
-	}`;
 
-	// create D1 Database for the worker
-	const resources = new Resources(c.env.CLOUDFLARE_ACCOUNT_ID, c.env.CLOUDFLARE_API_TOKEN);
-	const d1 = await resources.getOrCreateD1(user);
+		// create D1 Database for the worker
+		const resources = new Resources(c.env.CLOUDFLARE_ACCOUNT_ID, c.env.CLOUDFLARE_API_TOKEN);
+		const d1 = await resources.getOrCreateD1(user);
 
-	await scriptUpload.uploadScript(
-		namespace,
-		{
-			name: workerName,
-			script: {
-				mainFileName: 'index.js',
-				files: [
+		await scriptUpload.uploadScript(
+			namespace,
+			{
+				name: workerName,
+				script: {
+					mainFileName: validatedBody.mainFileName,
+					files: validatedBody.files.map((files) => ({
+						name: files.name,
+						content: Buffer.from(files.content),
+						type: files.type,
+					})),
+				},
+			},
+			{
+				tags: [`user:${user}`],
+				assets: {
+					jwt: assetsToken,
+				},
+				bindings: [
 					{
-						name: 'index.js',
-						content: Buffer.from(worker),
-						type: 'application/javascript+module',
+						type: 'assets',
+						name: 'ASSETS',
+					},
+					{
+						type: 'd1',
+						name: 'SQLITE',
+						id: d1.uuid,
 					},
 				],
-			},
-		},
-		{
-			tags: [`user:${user}`],
-			assets: {
-				jwt: assetsToken,
-			},
-			bindings: [
-				{
-					type: 'assets',
-					name: 'ASSETS',
+				observability: {
+					enabled: true,
 				},
-				{
-					type: 'd1',
-					name: 'SQLITE',
-					id: d1.uuid,
-				},
-			],
-			observability: {
-				enabled: true,
-			},
-			compatibility_date: '2025-06-20',
-			compatibility_flags: ['nodejs_compat'],
-		}
-	);
+				compatibility_date: '2025-06-20',
+				compatibility_flags: ['nodejs_compat'],
+			}
+		);
 
-	return c.text('Worker Uploaded!');
-});
+		return c.json({
+			ok: true,
+			errors: [],
+		});
+	}
+);
 
 export default app;
